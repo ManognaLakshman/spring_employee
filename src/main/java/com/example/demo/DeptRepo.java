7ea5473e129a539e03e6af7174f67abe52f22624
srikesh n <srikeshnagoji@gmail.com>
2018-11-15 10:35:34 +0530

    push to github

--- src/main/java/com/example/demo/DeptRepo.java
+++ src/main/java/com/example/demo/DeptRepo.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import java.util.Date;
 
+
+
+
 import java.util.List;
 
 import org.springframework.data.domain.Page;
@@ -11,9 +14,12 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.demo.model.Department;
 import com.example.demo.model.Employee;
+
+@CrossOrigin()
 @RepositoryRestResource(collectionResourceRel="departments",path="departments")
 public interface DeptRepo extends JpaRepository<Department, Integer> {
 	
