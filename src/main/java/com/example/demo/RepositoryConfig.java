42b22de342b2978b55b3d3422ee45fb16ef67f1d
srikesh n <srikeshnagoji@gmail.com>
2018-11-30 09:47:01 +0530

    changes in code to not display all links

--- src/main/java/com/example/demo/RepositoryConfig.java
+++ src/main/java/com/example/demo/RepositoryConfig.java
@@ -16,6 +16,7 @@ public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
     
     //
     config.setDefaultMediaType(MediaType.APPLICATION_JSON);
+    config.useHalAsDefaultJsonMediaType(false);
     //
   }
 }
