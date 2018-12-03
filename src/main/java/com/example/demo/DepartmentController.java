526712c7ba255f5e5b588c5643ed3bf8caf7347d
srikesh n <srikeshnagoji@gmail.com>
2018-12-03 12:43:23 +0530

    few lines added due to cors issue(heroku)

--- src/main/java/com/example/demo/DepartmentController.java
+++ src/main/java/com/example/demo/DepartmentController.java
@@ -16,6 +16,7 @@ import org.springframework.hateoas.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -31,7 +32,7 @@ import com.querydsl.core.types.Predicate;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
-
+@CrossOrigin()
 @EnableSpringDataWebSupport
 @RestController
 
--- src/main/java/com/example/demo/EmplRepo.java
+++ src/main/java/com/example/demo/EmplRepo.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import java.util.Date;
 
+
 import java.util.List;
 
 import org.springframework.data.domain.Page;
